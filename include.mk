0e9a76c30f41b3609229c141a64a6d5945aa9b32
Carl J. Nobile <carl.nobile@gmail.com>
2022-02-22 19:46:27 -0500

    Added a Makefile and the include.mk for version control.

---
+++ include.mk
@@ -0,0 +1,10 @@
+#
+# include.mk
+#
+
+# Restycode version and date info (DO NOT MODIFY)
+MAJORVERSION	= 0
+MINORVERSION	= 1
+PATCHLEVEL	= 0
+VERSION		= ${MAJORVERSION}.${MINORVERSION}.${PATCHLEVEL}
+#TODAY		= $(shell date +"%Y-%m-%d_%H%M")
