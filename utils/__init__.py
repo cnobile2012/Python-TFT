aa6beb1035a0627328e41fb98be04743f4580cdb
Carl J. Nobile <carl.nobile@gmail.com>
2022-04-08 11:33:13 -0400

    Opps, moved the __all__ in the utils/__init__.py.

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,9 +3,6 @@
 from .default_fonts import (Terminal6x8, Terminal11x16, Terminal12x16,
                             Trebuchet_MS16x21)
 
-__all__ = (Terminal6x8, Terminal11x16, Terminal12x16, Trebuchet_MS16x21,
-           TFTException, CompatibilityException)
-
 class TFTException(Exception):
     """
     Raised when an error is found in the main TFT class.
@@ -18,3 +15,6 @@ class CompatibilityException(Exception):
     Raised when there is a compatability error.
     """
     pass
+
+__all__ = (Terminal6x8, Terminal11x16, Terminal12x16, Trebuchet_MS16x21,
+           TFTException, CompatibilityException)
