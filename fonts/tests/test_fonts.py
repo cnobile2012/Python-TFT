3936fe1ec855d0f3ed6197fe2ddc15941d63f2c0
Carl J. Nobile <carl.nobile@gmail.com>
2022-04-21 22:06:54 -0400

    Added tests for the different parts of a GFX font file.

--- fonts/tests/test_fonts.py
+++ fonts/tests/test_fonts.py
@@ -45,15 +45,87 @@ class TestFonts(unittest.TestCase):
                       if not f.endswith(('__init__.py', 'font_convert.py'))]
         return font_files
 
-    def test_bitmap_in_fonts(self):
+    def test_bitmap_var_in_fonts(self):
         """
-        Test that the bitmap list exists in the fonts.
+        Test that the bitmaps list exists in the fonts and is not
+        zero length.
+        """
+        for name, module in self._modules.items():
+            bitmaps = []
+            var_name = f"{name}Bitmaps"
+
+            try:
+                bitmaps[:] = getattr(module, var_name)
+            except AttributeError as e:
+                msg - f"Font '{name}' does not have a '{var_name}' variable."
+                self.assertTrue(bitmaps, msg=msg)
+            else:
+                msg = f"Font '{name}', '{var_name}' variable is zero length."
+                self.assertNotEqual(len(bitmaps), 0, msg=msg)
+
+    def test_glyphs_var_in_fonts(self):
+        """
+        Test that the glyphs list exists in the fonts and it is not
+        zero length. Also test that the length of each gliph is correct.
 
         .. notes::
 
-            The bitmap variable has the following fields.
+            The glyphss variable has the following fields.
             [offset, width, height, advance cursor, x offset, y offset]
         """
         for name, module in self._modules.items():
-            bitmaps = getattr(module, "{name}Bitmaps")
-            self.assertFalse(True, msg="{name}: {len(bitmaps)}")
+            glyphs = []
+            glyph_size = 6
+            var_name = f"{name}Glyphs"
+
+            try:
+                glyphs[:] = getattr(module, var_name)
+            except AttributeError as e:
+                msg - f"Font '{name}' does not have a '{var_name}' variable"
+                self.assertTrue(bitmaps, msg=msg)
+            else:
+                tmp_msg = f("Font '{name}': should be {glyph_size} in "
+                            "length found: {{}}")
+
+                for lst in glyphs:
+                    found_size = len(lst)
+                    msg = tmp_msg.format(found_size)
+                    self.assertEqual(glyph_size, found_size, msg=msg)
+
+    def test_font_var_in_fonts(self):
+        """
+        Test that the font list exists in the fonts and it is not
+        zero length.
+
+        .. notes::
+
+            The font variable is a combination of the other two variables
+            plus three other values. The folowing fields should exist.
+            [bitmap var, glyph var, first extent, last extent, y advance]
+        """
+        for name, module in self._modules.items():
+            font = []
+            font_size = 5
+            var_name = name
+
+            try:
+                font[:] = getattr(module, var_name)
+            except AttributeError as e:
+                msg - f"Font '{name}' does not have a '{var_name}' variable"
+                self.assertTrue(bitmaps, msg=msg)
+            else:
+                found_size = len(font)
+                tmp_msg = (f"Font '{name}': should be {font_size} in "
+                           f"length found: {found_size}")
+                self.assertEqual(font_size, found_size, msg=msg)
+                tmp_msg = "{} item should be a {}, found {}"
+
+                for idx, item in enumerate(font, start=1):
+                    if idx < 2:
+                        num = f"{idx}{'st' if idx == 1 else 'nd'}"
+                        msg = tmp_msg.format(num, 'list', idx)
+                        self.assertTrue(isinstance(item, list), msg=msg)
+                    else:
+                        num = f"{idx}{'rd' if idx == 3 else 'th'}"
+                        msg = tmp_msg.format(num, 'int', idx)
+                        self.assertTrue(isinstance(item, int), msg=msg)
