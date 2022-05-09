7bccc917b55056baccd1e7f67c9e0c43b4933e6b
Carl J. Nobile <carl.nobile@gmail.com>
2022-05-08 21:13:54 -0400

    Added a run_test.sh to run specific tests.

---
+++ run_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Run a single test file with coverage.
+#
+
+PREFIX=$(pwd)
+COVERAGE_FILE=.coveragerc
+VE_BIN=$VIRTUAL_ENV/bin
+coverage erase --rcfile=${COVERAGE_FILE}
+export TFT_TESTING=True
+$VE_BIN/coverage run --rcfile=$(COVERAGE_FILE) $VE_BIN/nosetests $1
+coverage report -m --rcfile=${COVERAGE_FILE}
