641b2dba295eec69b77a269ed16b1f76c448be31
Carl J. Nobile <carl.nobile@gmail.com>
2022-05-08 21:17:29 -0400

    Fixed the run_test.sh file.

--- run_test.sh
+++ run_test.sh
@@ -8,5 +8,5 @@ COVERAGE_FILE=.coveragerc
 VE_BIN=$VIRTUAL_ENV/bin
 coverage erase --rcfile=${COVERAGE_FILE}
 export TFT_TESTING=True
-$VE_BIN/coverage run --rcfile=$(COVERAGE_FILE) $VE_BIN/nosetests $1
+${VE_BIN}/coverage run --rcfile=${COVERAGE_FILE} ${VE_BIN}/nosetests $1
 coverage report -m --rcfile=${COVERAGE_FILE}
