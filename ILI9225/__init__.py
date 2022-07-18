098e4178468c0aad8adb1307e79884f57a4c1673
Carl J. Nobile <carl.nobile@gmail.com>
2022-07-18 19:15:35 -0400

    Removed BGR16BitColor from the __init__.py.

--- ILI9225/__init__.py
+++ ILI9225/__init__.py
@@ -4,10 +4,10 @@ from .ili9225 import ILI9225
 from utils.default_fonts import (Terminal6x8, Terminal11x16, Terminal12x16,
                                  Trebuchet_MS16x21)
 from utils.common import (Boards, TFTException, CompatibilityException,
-                          RGB16BitColor, BGR16BitColor)
+                          RGB16BitColor)
 
 __all__ = (
     'ILI9225', 'Terminal6x8', 'Terminal11x16', 'Terminal12x16',
-    'Trebuchet_MS16x21', 'RGB16BitColor', 'BGR16BitColor', 'Boards',
-    'TFTException', 'CompatibilityException'
+    'Trebuchet_MS16x21', 'RGB16BitColor', 'Boards', 'TFTException',
+    'CompatibilityException'
     )
